cf1ecd67abf13db8bb6636d216a8151efe227b27
Judahezekiel <115859873+Judahezekiel@users.noreply.github.com>
2025-08-08 05:13:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # config.py
 
 # WhatsApp API settings
-WHATSAPP_API_URL = "https://graph.facebook.com/v19.0/<your_phone_number_id>/messages"
-WHATSAPP_TOKEN = "Bearer <your_whatsapp_api_token>"
-YOUR_WHATSAPP_NUMBER_ID = "<your_phone_number_id>"
+WHATSAPP_API_URL = "https://graph.facebook.com/v19.0/744771188719813/messages"
+WHATSAPP_TOKEN = "Bearer EAAKqFTeha2oBPIVGfReqcdwGTrRN5lbWpD3j6SZBWkDreE4c8Lnr0DG8OippMugpA1ZBLZCUeJWGVNODDE8OVGNZAyORJzAHBXZCUeZBXAHPQm37ZAqIHOn3nsUejf4SmSNbbt6jIzFTK1RXZASOPg0loQFryLhBaGZAlWBzWNwOcZCgdc5bSDK2bODUtgeoZAT6vpOQRXRlok7AZA9pc43JwIWb6haZAEDd5A11qu0dDQYZBNRw4KOwZDZD"
+YOUR_WHATSAPP_NUMBER_ID = "744771188719813"
 
 # Razorpay settings
 RAZORPAY_KEY_ID = "<your_razorpay_key_id>"
@@ -13,3 +13,4 @@ RAZORPAY_BASE_URL = "https://api.razorpay.com/v1"
 # Pricing
 DISCOUNT_THRESHOLD = 50  # ₹50
 DISCOUNT_PERCENTAGE = 10  # 10% off
+
